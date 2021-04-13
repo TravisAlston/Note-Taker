b81517569b8e9eb81f5545fb5e0ca5c81506194e
Valdeth <alstontravis6@gmail.com>
2021-04-12 23:47:22 -0600

    file/folder setup and initial apps and listeners

---
+++ server.js
@@ -0,0 +1,15 @@
+const expres = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () =>
+    console.log('server is running on ' + PORT)
+);
