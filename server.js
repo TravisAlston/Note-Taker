6b0e9c6cda60981f05e3e99e85e0f444f0bd043d
Valdeth <alstontravis6@gmail.com>
2021-04-13 00:13:02 -0600

    api routes functions added

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () =>
-    console.log('server is running on ' + PORT)
-);
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+});
