a5103a711fdc25770c86cf97fd6453f8dcc21ec2
Valdeth <alstontravis6@gmail.com>
2021-07-09 14:25:13 -0600

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,12 +5,12 @@ module.exports = function(app) {
     app.get('/notes', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
-
+    
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-
-    app.get('*', function(req, res){
+    
+    app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-};
+    };
